9e45a15451c048c2e03e3ee29183707193f8dd0f
David Millman <dave@cs.unc.edu>
2016-07-11 15:18:51 -0400

    Add test (before modifying calling)

---
+++ spec/synapse_pay_rest/http_client_spec.rb
@@ -0,0 +1,34 @@
+require 'rest-client'
+
+require_relative '../../lib/synapse_pay_rest/http_client.rb'
+
+RSpec.describe do
+  let(:client) { SynapsePayRest::HTTPClient.new({}, '') }
+
+  def self.it_calls_correct_rest_method(method, payload: nil)
+    it "calls the correct rest client method" do
+      allow(RestClient::Request).to receive(:execute).and_return('{}')
+      args = ['']
+      args << paylod if payload
+      client.method(method).call(*args)
+      expect(RestClient::Request).  to have_received(:execute).
+        with(hash_including(method: method))
+    end
+  end
+
+  describe "#post" do
+    it_calls_correct_rest_method :post, payload: {}
+  end
+
+  describe "#patch" do
+    it_calls_correct_rest_method :patch, payload: {}
+  end
+
+  describe "#get" do
+    it_calls_correct_rest_method :get
+  end
+
+  describe "#delete" do
+    it_calls_correct_rest_method :delete
+  end
+end
